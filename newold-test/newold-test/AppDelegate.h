c5a48862c502b3e85814633408be192c6ddc9620
Javier Soto Morras <jsotomorras@druss.ideo.com>
2016-09-13 11:12:18 +0100

    first commit from Javier

---
+++ newold-test/newold-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  newold-test
+//
+//  Created by Javier Soto Morras on 13/09/2016.
+//  Copyright © 2016 IDEO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ newold-test/newold-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  newold-test
+//
+//  Created by Javier Soto Morras on 13/09/2016.
+//  Copyright © 2016 IDEO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
